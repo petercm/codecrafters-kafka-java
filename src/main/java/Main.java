13c35ea9ae8ad2f855f880b639c597994716ef6b
Peter C Miller <petercm@gmail.com>
2024-11-17 10:19:41 -0500

    Format

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,37 +5,37 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
-  public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    System.err.println("Logs from your program will appear here!");
+    public static void main(String[] args){
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        System.err.println("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    // 
-     ServerSocket serverSocket = null;
-     Socket clientSocket = null;
-     int port = 9092;
-     try {
-       serverSocket = new ServerSocket(port);
-       // Since the tester restarts your program quite often, setting SO_REUSEADDR
-       // ensures that we don't run into 'Address already in use' errors
-       serverSocket.setReuseAddress(true);
-       // Wait for connection from client.
-       clientSocket = serverSocket.accept();
-       OutputStream out = clientSocket.getOutputStream();
-       DataOutputStream dos = new DataOutputStream(out);
-       dos.writeInt(0); // message_size
-       dos.writeInt(7); // correlation_id
-       dos.flush();
-     } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
-     } finally {
-       try {
-         if (clientSocket != null) {
-           clientSocket.close();
-         }
-       } catch (IOException e) {
-         System.out.println("IOException: " + e.getMessage());
-       }
-     }
-  }
+        // Uncomment this block to pass the first stage
+        //
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        int port = 9092;
+        try {
+            serverSocket = new ServerSocket(port);
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
+            // Wait for connection from client.
+            clientSocket = serverSocket.accept();
+            OutputStream out = clientSocket.getOutputStream();
+            DataOutputStream dos = new DataOutputStream(out);
+            dos.writeInt(0); // message_size
+            dos.writeInt(7); // correlation_id
+            dos.flush();
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        } finally {
+            try {
+                if (clientSocket != null) {
+                    clientSocket.close();
+                }
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            }
+        }
+    }
 }
