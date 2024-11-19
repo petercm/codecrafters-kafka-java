c58df32133d8f774b78573b5da1e36089e953ab2
Peter C Miller <peter.miller@atscale.com>
2024-11-19 13:09:23 -0500

    Handle multiple sequential requests from the same client

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,66 +17,73 @@ public class Main {
             serverSocket.setReuseAddress(true);
             // Wait for connection from client.
             clientSocket = serverSocket.accept();
-            DataInputStream dis = new DataInputStream(clientSocket.getInputStream());
+            InputStream in = clientSocket.getInputStream();
+            DataInputStream dis = new DataInputStream(in);
+            while (!clientSocket.isClosed()) {
+                /*
+                Request Header v2 => request_api_key request_api_version correlation_id client_id TAG_BUFFER
+                  request_api_key => INT16
+                  request_api_version => INT16
+                  correlation_id => INT32
+                  client_id => NULLABLE_STRING - For non-null strings, first the length N is given as an INT16.
+                    Then N bytes follow which are the UTF-8 encoding of the character sequence. A null value is
+                    encoded with length of -1
 
-            /*
-            Request Header v2 => request_api_key request_api_version correlation_id client_id TAG_BUFFER
-              request_api_key => INT16
-              request_api_version => INT16
-              correlation_id => INT32
-              client_id => NULLABLE_STRING - For non-null strings, first the length N is given as an INT16.
-                Then N bytes follow which are the UTF-8 encoding of the character sequence. A null value is
-                encoded with length of -1
+                */
+                int messageSize = dis.readInt();
+                System.out.println("Message size: " + messageSize);
+                ByteBuffer mis = ByteBuffer.wrap(dis.readNBytes(messageSize));
 
-            */
-            int messageSize = dis.readInt();
-            short apiKey = dis.readShort();
-            short requestApiVersion = dis.readShort();
-            int correlationId = dis.readInt();
-            short clientIdLength = dis.readShort();
-            String clientId = null;
-            if (clientIdLength >= 0) {
-                clientId = new String(dis.readNBytes(clientIdLength));
+                short apiKey = mis.getShort();
+                short requestApiVersion = mis.getShort();
+                int correlationId = mis.getInt();
+                short clientIdLength = mis.getShort();
+                String clientId = null;
+                if (clientIdLength >= 0) {
+                    clientId = new String(mis.get(new byte[clientIdLength]).array());
+                }
+                //TODO process tag buffer? Test calls have remaining bytes after extracting clientId
+                OutputStream out = clientSocket.getOutputStream();
+                ByteArrayOutputStream bos = new ByteArrayOutputStream();
+                DataOutputStream dos = new DataOutputStream(bos);
+                dos.writeInt(correlationId); // correlation_id
+
+                short errorCode = 0;
+
+                switch (apiKey) {
+                    case 18: // ApiVersions
+                        System.out.println("Received ApiVersions request " + requestApiVersion);
+                        if (requestApiVersion >= 0 && requestApiVersion <= 4) {
+                            /* ApiVersions Response (Version: 4) => error_code [api_keys] throttle_time_ms TAG_BUFFER
+                              error_code => INT16
+                              api_keys => api_key min_version max_version TAG_BUFFER
+                                api_key => INT16
+                                min_version => INT16
+                                max_version => INT16
+                              throttle_time_ms => INT32
+                              */
+                            dos.writeShort(0);
+                            dos.writeByte(2); // length+1 of the api_keys COMPACT_ARRAY
+                            dos.writeShort(18); // api_key
+                            dos.writeShort(0); // min_version
+                            dos.writeShort(4); // max_version
+                            dos.writeByte(0); // TAG_BUFFER
+                            dos.writeInt(0); // throttle_time_ms
+                            dos.writeByte(0); // TAG_BUFFER
+                        } else {
+                            errorCode = ApiErrors.UNSUPPORTED_VERSION.getErrorCode();
+                            dos.writeShort(errorCode);
+                        }
+                        break;
+                    default:
+                        //unsupported
+
+                }
+                dos.flush();
+                out.write(ByteBuffer.allocate(4).putInt(bos.size()).array()); // message_size
+                out.write(bos.toByteArray());
+                out.flush();
             }
-            OutputStream out = clientSocket.getOutputStream();
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            DataOutputStream dos = new DataOutputStream(bos);
-            dos.writeInt(correlationId); // correlation_id
-
-            short errorCode = 0;
-
-            switch (apiKey) {
-                case 18: // ApiVersions
-                    System.out.println("Received ApiVersions request " + requestApiVersion);
-                    if (requestApiVersion >= 0 && requestApiVersion <= 4) {
-                        /* ApiVersions Response (Version: 4) => error_code [api_keys] throttle_time_ms TAG_BUFFER
-                          error_code => INT16
-                          api_keys => api_key min_version max_version TAG_BUFFER
-                            api_key => INT16
-                            min_version => INT16
-                            max_version => INT16
-                          throttle_time_ms => INT32
-                          */
-                        dos.writeShort(0);
-                        dos.writeByte(2); // length+1 of the api_keys COMPACT_ARRAY
-                        dos.writeShort(18); // api_key
-                        dos.writeShort(0); // min_version
-                        dos.writeShort(4); // max_version
-                        dos.writeByte(0); // TAG_BUFFER
-                        dos.writeInt(0); // throttle_time_ms
-                        dos.writeByte(0); // TAG_BUFFER
-                    } else {
-                        errorCode = ApiErrors.UNSUPPORTED_VERSION.getErrorCode();
-                        dos.writeShort(errorCode);
-                    }
-                    break;
-                default:
-                    //unsupported
-
-            }
-            dos.flush();
-            out.write(ByteBuffer.allocate(4).putInt(bos.size()).array()); // message_size
-            out.write(bos.toByteArray());
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         } finally {
